fe6ecefe60e09c88765872a4602044f5abf659d0
Tiponi Williams <tiponiwilliams@VSUs-MacBook-Air.local>
2025-09-13 16:05:40 -0400

    Initial commit

---
+++ app/src/main/java/com/example/preworkhelloworld/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.preworkhelloworld
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.math.log
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.pressmeButton)
+        button.setOnClickListener {
+            Log.v("OUR ENEMIES", "Button clicked")
+            Toast.makeText(this, "OUR ENEMIES!", Toast.LENGTH_SHORT).show()
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
